4b466f9bf297d4295878705bd5fa38b5d6fc83fe
Louis Shinohara <louishinohara@gmail.com>
2019-07-14 07:37:58 +0900

    Finished Album. Started meet us section

--- js/custom.js
+++ js/custom.js
@@ -69,3 +69,15 @@ $(document).ready(function() {
     }
     );
 });
+
+/*========== TOP SCROLL BUTTON ==========*/
+
+$(document).ready(function() { //when document is ready
+    $(window).scroll(function() { //when webpage is scrolled
+      if ($(this).scrollTop() > 500) { //if scroll from top is more than 500
+        $('.top-scroll').fadeIn(); //display element with class 'top-scroll'; opacity increases
+      } else { //if not
+        $('.top-scroll').fadeOut(); //hide element with class 'top-scroll'; opacity decreases
+      }
+    });
+  });
